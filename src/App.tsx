5861b8d91aa4c059747943318695fd42ba089784
Hanyu-Lin <hanyulin02@gmail.com>
2022-12-25 17:59:22 +0100

    Added CustomForm and updated index.css

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,33 @@
-import { useState } from 'react'
+import React, { Component } from 'react';
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <div className="App">
-    </div>
-  )
+// custom components
+import CustomForm from './components/CustomForm';
+import { Task } from './components/TaskType';
+interface State {
+  tasks: Task[],
 }
 
-export default App
+class App extends Component<{}, State> {
+  state: State = {
+    tasks: [],
+  };
+
+  addTask = (task: Task) => {
+    this.setState(prevState => ({ tasks: [...prevState.tasks, task] }));
+  }
+
+  render() {
+    const { tasks } = this.state;
+    return (
+      <div className="container">
+        <header>
+          <h1>My Todo List Tracker</h1>
+        </header>
+        <CustomForm addTask={this.addTask} />
+        {/* {tasks.length > 0 && <TaskList tasks={tasks} />} */}
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/CustomForm.tsx
@@ -0,0 +1,68 @@
+import React, { Component } from 'react';
+import { PlusIcon } from '@heroicons/react/24/solid';
+import { Task } from './TaskType';
+
+interface Props {
+  addTask: (task: Task) => void;
+}
+
+class CustomForm extends Component<Props> {
+  state = {
+    task: '',
+  }
+
+  handleFormSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const { addTask } = this.props;
+    const { task } = this.state;
+    addTask({
+      name: task,
+      checked: false,
+      id: Date.now(),
+    });
+    this.setState({ task: '' });
+  }
+
+  handleInput = (e: React.FormEvent<HTMLInputElement>) => {
+    this.setState({ task: (e.target as HTMLInputElement).value });
+  }
+
+  render() {
+    const { task } = this.state;
+    return (
+      <form
+        className="todo"
+        onSubmit={this.handleFormSubmit}
+      >
+        <div className="wrapper">
+          <input
+            type="text"
+            className="input"
+            id="task"
+            value={task}
+            onInput={this.handleInput}
+            required
+            autoFocus
+            maxLength={100}
+            placeholder="Enter Task"
+          />
+          <label
+            htmlFor="task"
+            className="label"
+          >
+            Enter Task
+          </label>
+        </div>
+        <button
+          className="btn"
+          aria-label="Add Task"
+          type="submit"
+        >
+          <PlusIcon />
+        </button>
+      </form>
+    );
+  }
+}
+
+export default CustomForm;
