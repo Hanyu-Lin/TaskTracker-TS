8db2b41d2fa6e46a5dd497797fdd8a0208bc2e04
Hanyu-Lin <hanyulin02@gmail.com>
2022-12-27 11:19:35 +0100

    Added support for localStorage hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-
+import React, { useState } from 'react';
+import useLocalStorage from './hooks/useLocalStorage';
 // custom components
 import CustomForm from './components/CustomForm';
 import TaskList from './components/TaskList';
@@ -11,70 +11,72 @@ const tempTask: Task = {
   checked: false,
   id: ""
 }
-class App extends React.Component {
-  state = {
-    tasks: [] as Task[],
-    previousFocusEl: document.createElement('div'),
-    editedTask: tempTask,
-    isEditing: false
-  };
 
-  addTask = (task: Task) => {
-    this.setState({tasks : [...this.state.tasks,task]});
+const App = () => {
+  const [tasks, setTasks] = useLocalStorage<Task[]>('react-todo.tasks', []);
+  const [previousFocusEl, setPreviousFocusEl] = useState(document.createElement('div') as HTMLElement);
+  const [editedTask, setEditedTask] = useState(tempTask);
+  const [isEditing, setIsEditing] = useState(false);
+
+  const addTask = (task: Task) => {
+    setTasks([...tasks, task]);
   }
-  deleteTask = (id: string) => {
-    this.setState({tasks : this.state.tasks.filter(task => task.id !== id)})
+  const deleteTask = (id: string) => {
+    setTasks(tasks.filter(task => task.id !== id));
   }
-  toggleTask = (id: string) => {
-    this.setState({tasks : this.state.tasks.map(
-        task => task.id === id ? {...task, checked: !task.checked } : task
-      )})
+  const toggleTask = (id: string) => {
+    setTasks(tasks.map(
+      task => task.id === id ? {...task, checked: !task.checked } : task
+    ));
   }
-  updateTask = (task: Task) => {
-    this.setState({tasks: this.state.tasks.map( t => (
+  const updateTask = (task: Task) => {
+    setTasks(tasks.map( t => (
       t.id === task.id 
       ?{ ...t, name: task.name }
       : t
-    ))});
-    this.closeEditMode();
+    )));
+    closeEditMode();
   }
-  closeEditMode = () => {
-    this.setState({ isEditing: false });
-    if (this.state.previousFocusEl) {
-      this.state.previousFocusEl.focus();
+  const closeEditMode = () => {
+    setIsEditing(false);
+    if (previousFocusEl) {
+      previousFocusEl.focus();
     }
   }
-  enterEditMode = (task: Task) => {
-    this.setState({ editedTask: task, isEditing: true, previousFocusEl: document.activeElement });
+  const enterEditMode = (task: Task) => {
+    setEditedTask(task);
+    setIsEditing(true);
+    if (document.activeElement instanceof HTMLElement) {
+      setPreviousFocusEl(document.activeElement);
+    }
   }
 
-  render() {
-    const { tasks, isEditing, editedTask } = this.state;
-    return (
-      <div className="container">
-        <header>
-          <h1>My Todo List Tracker</h1>
-        </header>
-        {
-          isEditing && (
-            <EditForm
-              editedTask={editedTask}
-              updateTask={this.updateTask}
-              closeEditMode={this.closeEditMode}
-            />
-          )
-        }
-        <CustomForm addTask={this.addTask} /> 
-        {this.state.tasks && 
-          <TaskList 
-            tasks={this.state.tasks} 
-            deleteTask ={this.deleteTask}
-            toggleTask ={this.toggleTask}
-            enterEditMode={this.enterEditMode}
-          />}
-      </div>
-    )
-  }
+
+  return (
+    <div className="container">
+      <header>
+        <h1>My Todo List Tracker</h1>
+      </header>
+      {
+        isEditing && (
+          <EditForm
+            editedTask={editedTask}
+            updateTask={updateTask}
+            closeEditMode={closeEditMode}
+          />
+        )
+      }
+      <CustomForm addTask={addTask} /> 
+      {tasks.length > 0 && 
+        <TaskList 
+          tasks={tasks} 
+          deleteTask ={deleteTask}
+          toggleTask ={toggleTask}
+          enterEditMode={enterEditMode}
+        />}
+    </div>
+  );
 }
+
 
 export default App;
